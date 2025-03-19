66dae20d4d294a7c21e5256ac58eb66e00ac1c8b
zuzia zolich <zuzanna.zolich@zse.krakow.pl>
2025-03-19 13:25:53 +0000

    calculator

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -2,3 +2,21 @@
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+
+#[ic_cdk::query]
+fn calculate(a: i32, b: i32, operator: String) -> String {
+    let result = match operator.as_str() {
+        "+" => Some(a + b),
+        "-" => Some(a - b),
+        "*" => Some(a * b),
+        "/" => if b != 0 { Some(a / b) } else { None },
+        "%" => if b != 0 { Some(a % b) } else { None },
+        _ => None,
+    };
+
+    match result {
+        Some(value) => format!("Result: {}", value),
+        None => "Wrong operator or division by zero".to_string(), 
+    }
+}
